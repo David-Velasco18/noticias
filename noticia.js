19e7e61e81715c865e1ae2ea12030b3e89f13465
David-Velasco18 <24010401@es.uveg.edu.mx>
2026-09-23 20:36:25 -0600

    Add files via upload

---
+++ noticia.js
@@ -0,0 +1,34 @@
+const pantalla = document.getElementById('noticias');
+pantalla.innerHTML = "<p>Cargando... </p>";
+
+const boton = document.getElementById('boton-refrescar');
+
+boton.addEventListener ('click', cargarnoticias);
+function cargarnoticias(){
+    console.log("Botón conectado");
+    const url = 'https://gnews.io/api/v4/top-headlines?category=general&lang=es&apikey=bb012ed3567af30afd984601e626332f';
+    fetch (url)
+    .then(response => response.json())
+    .then(data => {console.log(data)
+       
+        pantalla.innerHTML = "";
+        
+        data.articles.forEach(noticia => {
+
+        pantalla.innerHTML += `<div class="tarjeta-noticia">
+        
+            <img src="${noticia.image}" class="foto-noticia">
+            <h3><a href="${noticia.url}" target="_blank"> ${noticia.title}</a></h3>
+            <p>${noticia.description}</p>
+            <p>${noticia.publishedAt}</p>
+
+        </div>`;
+
+    });
+    })
+    .catch(error => console.error ('Error', error));
+    
+}
+
+cargarnoticias();
+
